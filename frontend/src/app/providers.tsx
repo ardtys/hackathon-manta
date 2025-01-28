bbed88987797172a63ca21bb7d6e235886a30de0
yoghantara08 <gyoghantara@gmail.com>
2025-01-28 22:32:22 +0800

    feat: add RainbowKit integration and configure wagmi

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -3,16 +3,30 @@
 import React from "react";
 import { Provider } from "react-redux";
 
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PersistGate } from "redux-persist/integration/react";
+import { WagmiProvider } from "wagmi";
 
+import wagmiConfig from "@/config/wagmiConfig";
 import { persistor, store } from "@/redux/store";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        {children}
-      </PersistGate>
-    </Provider>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              {children}
+            </PersistGate>
+          </Provider>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
---
+++ frontend/src/config/wagmiConfig.ts
@@ -0,0 +1,15 @@
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { mantaSepoliaTestnet } from "wagmi/chains";
+
+import { PROJECT_ID } from "@/constant";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+const wagmiConfig = getDefaultConfig({
+  appName: "Zellar",
+  projectId: PROJECT_ID,
+  chains: [mantaSepoliaTestnet],
+  ssr: true,
+});
+
+export default wagmiConfig;
